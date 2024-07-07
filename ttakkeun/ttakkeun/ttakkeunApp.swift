edf7b4c505f066410a780d1a6eb5f57ad12f3975
JEONG-J <ikejhc159@gmail.com>
2024-07-07 15:35:48 +0900

    📝 깃 이슈 템플릿, 깃 풀리퀘 템플릿 작성완료
    이슈 템플릿, 풀리퀘 템플릿, 이그노어 파일 작성

---
+++ ttakkeun/ttakkeun/ttakkeunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttakkeunApp.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ttakkeunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
