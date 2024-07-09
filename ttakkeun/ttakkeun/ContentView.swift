8a73d35ffe7f98be8c19a08496f4508a4dae8ee8
JEONG-J <ikejhc159@gmail.com>
2024-07-09 15:23:30 +0900

    ✨ 폰트 모디피어 생성
    SUIT 폰트 모디피어 생성

--- ttakkeun/ttakkeun/ContentView.swift
+++ ttakkeun/ttakkeun/ContentView.swift
@@ -14,6 +14,15 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .onAppear {
+                    UIFont.familyNames.sorted().forEach { familyName in
+                        print("*** \(familyName) ***")
+                        UIFont.fontNames(forFamilyName: familyName).forEach { fontName in
+                            print("\(fontName)")
+                        }
+                        print("---------------------")
+                    }
+                }
         }
         .padding()
     }
---
+++ ttakkeun/ttakkeun/Fonts/Font.swift
@@ -0,0 +1,50 @@
+//
+//  Font.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 7/9/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    enum Suit {
+        case light
+        case bold
+        case extraBold
+        case extraLight
+        case heavy
+        case medium
+        case regular
+        case semibold
+        case thin
+        
+        var value: String {
+            switch self {
+            case .regular:
+                return "SUIT-Regular"
+            case .thin:
+                return "SUIT-Thin"
+            case .extraLight:
+                return "SUIT-ExtraLight"
+            case .light:
+                return "SUIT-Light"
+            case .medium:
+                return "SUIT-Medium"
+            case .semibold:
+                return "SUIT-SemiBold"
+            case .bold:
+                return "SUIT-Bold"
+            case .extraBold:
+                return "SUIT-ExtraBold"
+            case .heavy:
+                return "SUIT-Heavy"
+            }
+        }
+    }
+    
+    static func suit(type: Suit, size: CGFloat) -> Font {
+        return .custom(type.value, size: size)
+    }
+}
