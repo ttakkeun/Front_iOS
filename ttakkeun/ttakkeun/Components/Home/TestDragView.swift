3481df7dc9b29341bf7d414f9c3cc23962cdf59e
JEONG-J <ikejhc159@gmail.com>
2024-07-17 22:00:28 +0900

    ✨ 홈탭 드래그 뷰 구현
    offset으로 뷰의 높이를 조절하여 사용할 수 있는 드래그 뷰 작성

---
+++ ttakkeun/ttakkeun/Components/Home/TestDragView.swift
@@ -0,0 +1,18 @@
+//
+//  TestDragView.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 7/17/24.
+//
+
+import SwiftUI
+
+struct TestDragView: View {
+    var body: some View {
+        HomeDragView()
+    }
+}
+
+#Preview {
+    TestDragView()
+}
