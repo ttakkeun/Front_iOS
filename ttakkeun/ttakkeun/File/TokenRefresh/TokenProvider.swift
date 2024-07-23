4a1dd4c80dbd602814b20c0e07893aab98e2f968
JEONG-J <ikejhc159@gmail.com>
2024-07-23 20:13:16 +0900

    ✨ 프로필 관련 API 타겟 생성
    프로필 관련 API 타켓 생성

--- ttakkeun/ttakkeun/File/TokenRefresh/TokenProvider.swift
+++ ttakkeun/ttakkeun/File/TokenRefresh/TokenProvider.swift
@@ -32,15 +32,15 @@ class TokenProvider: TokenProviding {
     
     /* 리프레시 토큰 데이터 담아서 보내야 함 */
     func refreshToken(completion: @escaping (String?, Error?) -> Void) {
-        guard let userInfo = keyChain.loadSession(for: "userSession") else { return }
+        guard var userInfo = keyChain.loadSession(for: "userSession") else { return }
         
         provider.request(.refreshToken(refreshToken: userInfo.refreshToken ?? "")) { result in
             switch result {
             case .success(let response):
                 do {
                     let tokenData = try JSONDecoder().decode(TokenResponse.self, from: response.data)
-                    self.accessToken = tokenData.accessToken
-                    completion(tokenData.accessToken, nil)
+                    self.accessToken = tokenData.result.accessToken
+                    completion(self.accessToken, nil)
                 } catch {
                     completion(nil, error)
                 }
