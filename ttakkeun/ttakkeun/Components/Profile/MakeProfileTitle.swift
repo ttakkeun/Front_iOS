394a3b4a63586d1602758db177e9df923ff692bc
alice0047@cau.ac.kr <alicee0047@gmail.com>
2024-07-29 17:01:20 +0900

    ♻️ 프로필 생성 뷰 네임태그 컴포넌트 수정
    프로필 생성 뷰 네임태그 컴포넌트 수정

--- ttakkeun/ttakkeun/Components/Profile/MakeProfileTitle.swift
+++ ttakkeun/ttakkeun/Components/Profile/MakeProfileTitle.swift
@@ -12,11 +12,21 @@ struct MakeProfileNameTag: View {
     var titleText: String
     var mustMark: Bool
     
+    //MARK: - Init
+    
+    /// Description
+    /// - Parameters:
+    ///   - titleText: 네임태그 텍스트
+    ///   - width: 331
+    ///   - height: 20
+    ///   - color: .gray_900
     init(titleText: String, mustMark: Bool) {
         self.titleText = titleText
         self.mustMark = mustMark
     }
     
+    //MARK: - Contents
+    /// 네임태그 기본적인 구조
     var body: some View {
         HStack(alignment: .top, spacing: 2, content: {
             Text(titleText)
