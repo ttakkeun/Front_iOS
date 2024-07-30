51f7fd500bfca89b78566fddb1e997d00547aa20
JEONG-J <ikejhc159@gmail.com>
2024-07-30 16:47:07 +0900

    💄 홈 탭 컴포넌트 완성
    홈 탭에 필요한 컴포넌트 작성 완료

--- ttakkeun/ttakkeun/DataModelGroup/Product/AIRecommendProductData.swift
+++ ttakkeun/ttakkeun/DataModelGroup/Product/AIRecommendProductData.swift
@@ -7,24 +7,11 @@
 
 import Foundation
 
-struct AIRecommendProductData: Codable {
+struct AIAndSearchProductData: Codable {
     let isSuccess: Bool
     let code: Int
     let message: String
-    let result: [AIRecommendProductResponseData]
-}
-
-struct AIRecommendProductResponseData: Codable, Hashable {
-    let likeStatus: Bool
-    let totalLike: Int
-    let product: [ProductDetailData]
-    
-    enum CodingKeys: String, CodingKey {
-        
-        case likeStatus = "likes_status"
-        case totalLike = "total_likes"
-        case product
-    }
+    let result: [ProductDetailData]
 }
 
 struct ProductDetailData: Codable, Hashable {
@@ -34,14 +21,21 @@ struct ProductDetailData: Codable, Hashable {
     let price: Int
     let brand: String
     let purchaseLink: String
-    let category1: String
-    let category2: String
-    let category3: String
-    let category4: String
+    let category1: String?
+    let category2: String?
+    let category3: String?
+    let category4: String?
+    
+    let totalLike: Int?
+    let likeStatus: Bool
+    
     
     enum CodingKeys: String, CodingKey {
         case productId = "product_id"
         case title, image, price, brand, category1, category2, category3, category4
         case purchaseLink = "link"
+        case totalLike = "total_likes"
+        case likeStatus = "isLike"
+        
     }
 }
