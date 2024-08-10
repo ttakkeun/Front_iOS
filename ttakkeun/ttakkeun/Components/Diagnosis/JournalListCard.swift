17c663503318e51921bf553cc9aeba3c03e369ca
JEONG-J <ikejhc159@gmail.com>
2024-08-10 16:38:19 +0900

    일지 조회 카드 생성 및 파일 분리
    일지 조회 카드 생성 및 파일 분리 생성

---
+++ ttakkeun/ttakkeun/Components/Diagnosis/JournalListCard.swift
@@ -0,0 +1,19 @@
+//
+//  JournalListCard.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 8/10/24.
+//
+
+import SwiftUI
+
+/// 조회된 일지 목록 카드
+struct JournalListCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalListCard()
+}
---
+++ ttakkeun/ttakkeun/ViewGroup/Diagnosis/JournalListView.swift
@@ -0,0 +1,19 @@
+//
+//  JournalListView.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 8/10/24.
+//
+
+import SwiftUI
+
+/// 일지 목록 바디 뷰, 조회된 일지 리스트 불러온다.
+struct JournalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalListView()
+}
