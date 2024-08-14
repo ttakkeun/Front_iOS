7ec504ceef19c44afc38e7525b9532660f134e9f
alice0047@cau.ac.kr <alicee0047@gmail.com>
2024-08-14 15:42:51 +0900

    ♻️ 각 동의항목에 대한 세부사항 데이터 정리
    각 동의항목(서비스 이용약관 동의, 개인정보 수집 및 이용동의, 마케팅 수신정보 이용동의)에 대한 세부 사항 데이터 정리

--- ttakkeun/ttakkeun/ViewModelGroup/AgreementViewModel.swift
+++ ttakkeun/ttakkeun/ViewModelGroup/AgreementViewModel.swift
@@ -12,38 +12,41 @@ class AgreementViewModel: ObservableObject {
     @Published var agreements: [AgreementData] = []
     @Published var selectedAgreement: AgreementData?
     
+    //MARK: - INIT
     init() {
         loadAgreements()
     }
     
-    // 모든 항목이 체크되었는지 확인
+    //MARK: - Variable
+    /// 모든 항목이 체크되었는지 확인
     var isAllChecked: Bool {
         agreements.allSatisfy { $0.isChecked }
     }
     
-    // 필수 항목들이 모두 체크되었는지 확인
+    /// 필수 항목들이 모두 체크되었는지 확인
     var isAllMandatoryChecked: Bool {
         agreements.filter { $0.isMandatory }.allSatisfy { $0.isChecked }
     }
     
+    //MARK: - Function
+    /// 특정 동의 항목의 체크 상태를 토글하는 함수
     func toggleCheck(for item: AgreementData) {
         if let index = agreements.firstIndex(where: { $0.id == item.id }) {
             agreements[index].isChecked.toggle()
         }
     }
     
+    /// 모든 동의 항목의 체크 상태를 토글하는 함수
     func toggleAllAgreements() {
+        /// 현재 모든 항목이 체크되었는지 여부에 따라 새로운 값을 설정
         let newValue = !isAllChecked
         for index in agreements.indices {
             agreements[index].isChecked = newValue
         }
     }
     
+    /// 동의 항목 데이터를 로드하는 함수
     private func loadAgreements() {
-        agreements = [
-            AgreementData(title: "서비스 이용 약관 동의 (필수)", isMandatory: true, detailText: "서비스 이용에 대한 약관 내용..."),
-            AgreementData(title: "개인정보 수집 및 이용 동의 (필수)", isMandatory: true, detailText: "개인정보 수집 및 이용에 대한 약관 내용..."),
-            AgreementData(title: "마케팅 정보 수신 동의 (선택)", isMandatory: false, detailText: "마케팅 정보 수신에 대한 약관 내용...")
-        ]
+        agreements = AgreementDetailData.loadAgreements()
     }
 }
