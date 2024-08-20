0b5d89086e5ee2d5a420b1321bf2fae2d23075c1
hanjk0816 <hanjk0816@naver.com>
2024-08-20 23:30:31 +0900

    💄 상품ProductSheet뷰 수정
    데이터 수정된 것 바탕으로 불러오도록 함

--- ttakkeun/ttakkeun/Components/Recommend/ProductSheetView.swift
+++ ttakkeun/ttakkeun/Components/Recommend/ProductSheetView.swift
@@ -5,16 +5,21 @@
 //  Created by 한지강 on 7/22/24.
 //
 
-
 import SwiftUI
 import Kingfisher
 
+//TODO: - RecommendProduct 눌렀을 때 sheet로 띄워야 함.
 struct ProductSheetView: View {
     
     let data: RecommenProductResponseData
     
+    @State private var isLike: Bool
+    @State private var totalLikes: Int
+    
     init(data: RecommenProductResponseData) {
         self.data = data
+        _isLike = State(initialValue: data.isLike)
+        _totalLikes = State(initialValue: data.total_likes)
     }
     
     //MARK: - Contents
@@ -22,20 +27,22 @@ struct ProductSheetView: View {
         VStack(spacing: 28){
             
             Capsule()
-                .fill(Color.gray)
+                .fill(Color.gray300)
                 .frame(width: 38, height: 5)
                 .padding(.top, 10)
             
             productImage
-                .border(Color.gray)
+                .border(Color.gray200)
             
             titleset
-            
+            //TODO: - 버튼 눌렀을 때 상품페이지 링크로 넘어가야 함.
             MainButton(btnText: "구매하러가기", width: 342, height: 59, action: {
-                print("구매하기 버튼 클릭됨")
-            }, color: .blue)
+                // 상품 구매 페이지로 이동하는 로직 구현
+                if let url = URL(string: data.link) {
+                    UIApplication.shared.open(url)
+                }
+            }, color: .primaryColorMain)
             
-            Spacer()
         }
         .frame(maxWidth: 394)
     }
@@ -55,13 +62,14 @@ struct ProductSheetView: View {
     }
     
     /// 이미지
+    //TODO: - 상품이미지 데이터 받아와서 띄워야 함.
     @ViewBuilder
     private var productImage: some View {
         if let url = URL(string: data.image) {
             KFImage(url)
                 .placeholder{
                     ProgressView()
-                        .frame(width: 100,height: 100)
+                        .frame(width: 100, height: 100)
                 }.retry(maxCount: 2, interval: .seconds(2))
                 .onFailure { _ in
                     print("sheetView에 상품이미지 불러오기 실패")
@@ -73,68 +81,84 @@ struct ProductSheetView: View {
     }
     
     /// 카테고리
+    //TODO: - 카테고리 받아와서 띄워야 함.
     private var category: some View {
         HStack {
             Text("\(data.category2) > \(data.category3) > \(data.category4)")
                 .font(.system(size: 14, weight: .semibold))
-                .foregroundColor(Color.gray)
+                .foregroundColor(Color.gray400)
             
             Spacer()
             
+            //TODO: - 공유하기 버튼 기능 추가
             Button(action:{
+                // 공유하기 기능 추가 로직 구현
                 print("공유하기 버튼 클릭됨")
             }, label: {
                 Image(systemName: "square.and.arrow.up")
-                    .foregroundColor(Color.gray)
+                    .foregroundColor(Color.gray400)
             })
         }
     }
     
     /// 타이틀
+    //TODO: - 상품이름 받아서 띄워야 함.
     private var title: some View {
         Text(data.title)
             .frame(maxWidth: 325, alignment: .leading)
             .font(.system(size: 16, weight: .semibold))
-            .foregroundColor(Color.black)
+            .foregroundColor(Color.gray900)
             .multilineTextAlignment(.leading)
             .lineLimit(nil)
     }
     
     /// 기업명
+    //TODO: - 기업이름 받아와서 띄워야 함.
+    /// 기업이름
     private var mallname: some View {
         Text("\(data.brand) >")
             .font(.system(size: 12, weight: .medium))
-            .foregroundColor(Color.gray)
+            .foregroundColor(Color.gray900)
     }
     
-    /// 하트수와 가격
+    //TODO: - 상품가격 받아와서 띄워야 함.
+    /// 상품가격
     private var productprice: some View {
         HStack{
-            //하트버튼과 하트수
-            Button(action: {
-                print("하트 버튼 클릭됨")
-            }, label: {
-                HStack{
-                    Image(systemName: "heart.fill")
-                        .foregroundColor(Color.red)
-                    Text("\(data.total_likes)")
-                        .foregroundColor(Color.red)
-                }
-            })
+            heartBtn
             Spacer()
-            //가격
             Text("\(data.price)원")
                 .font(.system(size: 18, weight: .bold))
+                .foregroundColor(Color.gray900)
             
         }
         .frame(width: 342, height: 24)
+    }
+    
+    //TODO: - 하트 isLike(Bool)이랑, 하트수 totalNumber(Int) 받아와서 띄워야 함, 그리고 누르면 patch로 하트수 변경할 수 있어야 함.
+    // 하트 버튼과 하트 수
+    private var heartBtn: some View {
+        HStack{
+            Button(action: {
+                isLike.toggle()
+                totalLikes += isLike ? 1 : -1
+                // 여기에 서버와 통신하여 하트 상태를 업데이트하는 로직 추가 (PATCH 요청)
+            }, label: {
+                HStack{
+                    Image(systemName: isLike ? "heart.fill" : "heart")
+                        .foregroundColor(isLike ? Color.red : Color.gray)
+                    Text("\(totalLikes)")
+                        .foregroundColor(isLike ? Color.red : Color.gray)
+                }
+            })
+        }
     }
 }
 
 //MARK: - Preview
 struct ProductSheetView_Preview: PreviewProvider {
     static var previews: some View {
-        let sampleProduct = RecommenProductResponseData(
+        ProductSheetView(data: RecommenProductResponseData(
             product_id: 1,
             title: "피부병 아토비 각질 비듬 예방 아껴주다 저자극 천연 고양이 샴푸 500ml(고양이 비듬, 턱드름 관리)",
             image: "https://shopping-phinf.pstatic.net/main_3596342/35963422008.jpg",
@@ -147,16 +171,7 @@ struct ProductSheetView_Preview: PreviewProvider {
             category4: "샴푸, 린스",
             total_likes: 304,
             isLike: true
-        )
-
-        let sampleResponse = ResponseData(
-            isSuccess: true,
-            code: "200",
-            message: "Success",
-            result: sampleProduct
-        )
-
-        ProductSheetView(data: sampleResponse.result!)
-            .previewLayout(.sizeThatFits)
+        ))
+        .previewLayout(.sizeThatFits)
     }
 }
