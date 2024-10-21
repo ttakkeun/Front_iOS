b8492e3388681af6bd1d295bba3a0111b9ceaa0c
JEONG-J <ikejhc159@gmail.com>
2024-10-21 15:25:14 +0900

    🚑️ 정식 버전 프로젝트 생성
    프로젝트 파일 생성 및 파일 재구축

---
+++ ttakkeun/ttakkeun/API/Util/Config.swift
@@ -0,0 +1,24 @@
+//
+//  Config.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 10/21/24.
+//
+
+import Foundation
+
+enum Config {
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist cannot be fount")
+        }
+        return dict
+    }()
+    
+    static let baseURL: String = {
+        guard let baseURL = Config.infoDictionary["BASE_URL"] as? String else {
+            fatalError("BaseURL not found")
+        }
+        return baseURL
+    }()
+}
