e6726375646571189e9f71301e97c5044dc2044d
JEONG-J <ikejhc159@gmail.com>
2024-10-24 01:41:17 +0900

    ♻️ Ignore 수정및 프로젝트 파일 수정
    ignore 프로젝트 파일 숨김 해제 및 프로젝트 파일 수정

--- ttakkeun/ttakkeun/ContentView.swift
+++ ttakkeun/ttakkeun/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  ttakkeun
 //
-//  Created by 정의찬 on 10/21/24.
+//  Created by 정의찬 on 10/24/24.
 //
 
 import SwiftUI
---
+++ ttakkeun/ttakkeun/ttakkeunApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttakkeunApp.swift
+//  ttakkeun
+//
+//  Created by 정의찬 on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ttakkeunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
