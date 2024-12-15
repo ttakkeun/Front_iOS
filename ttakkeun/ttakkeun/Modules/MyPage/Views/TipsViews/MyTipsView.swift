1acccc91050d74e5ebb9db7c2a79b9bd95a40541
alice0047@cau.ac.kr <alicee0047@gmail.com>
2024-12-15 16:41:11 +0900

    💄 내가 쓴 Tips 최종 수정
    내가 쓴 Tips 최종 수정

--- ttakkeun/ttakkeun/Modules/MyPage/Views/TipsViews/MyTipsView.swift
+++ ttakkeun/ttakkeun/Modules/MyPage/Views/TipsViews/MyTipsView.swift
@@ -12,7 +12,7 @@ struct MyTipsView: View {
     @State var response: [TipsResponse]
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center, spacing: 30, content: {
             CustomNavigation(action: { print("hello world") },
                              title: "내가 쓴 tips",
                              currentPage: nil,
@@ -20,21 +20,24 @@ struct MyTipsView: View {
                              width: 8,
                              height: 16)
             
-            ScrollView(.vertical) {
-                LazyVStack(spacing: 20) {
-                    ForEach($response, id: \.id) { $tip in
-                        TipsContentsCard(
-                            data: $tip,
-                            tipsType: .writeMyTips,
-                            tipsButtonOption: TipsButtonOption(
-                                heartAction: { print("Heart action") },
-                                scrapAction: { print("Scrap action") }
-                            ),
-                            showReportBtn: false
-                        )
+            
+            ScrollView(.vertical, content: {
+                contents
+            })
+        })
+    }
+    
+    /// 내가 쓴 tips 콘텐츠들
+    private var contents: some View {
+        LazyVStack(spacing: 20) {
+            ForEach($response, id: \.id) { $tip in
+                TipsContentsCard(
+                    data: $tip,
+                    tipsType: .writeMyTips,
+                    deleteTipsAction: {
+                        print("Delete button tapped for \(tip.title)")
                     }
-                }
-                .padding()
+                )
             }
         }
     }
@@ -43,7 +46,9 @@ struct MyTipsView: View {
 extension MyTipsView {
     static var sampleTips: [TipsResponse] {
         [
-            TipsResponse(tipId: 1, category: .ear, title: "Tip 1", content: "Content for tip 1", recommendCount: 10, createdAt: "24.10.11", imageUrls: ["https://cdn.news.unn.net/news/photo/202110/516864_318701_956.jpg", "https://cdn.news.unn.net/news/photo/202110/516864_318701_956.jpg"], authorName: "User", isLike: false, isPopular: true, isScrap: true)
+            TipsResponse(tipId: 0, category: .ear, title: "Tip 1", content: "Content for tip 1", recommendCount: 50, createdAt: "2024-11-14T05:20:00.08Z", imageUrls: ["https://cdn.news.unn.net/news/photo/202110/516864_318701_956.jpg", "https://cdn.news.unn.net/news/photo/202110/516864_318701_956.jpg"], authorName: "황유빈", isLike: false, isPopular: true, isScrap: true),
+            
+            TipsResponse(tipId: 1, category: .claw, title: "털 안꼬이게 빗는 법 꿀팁 공유 드림", content: "털은 빗어주지 않으면 쉽게 꼬일 수 있기 때문에 열심히 빗어주면 됩니다. 그래서 매일매일 빗질을 해주는 것이 매우 중요합니다. 털의 길이와 상태에 따라 길이와 상태에 따라 종류에 맞는 빗을 구매하셔서 부드럽게 털을 빗어 죽은 털을 제거해주세요 그러면 윤기나고 건강한 털을 유지할 수 있습니다. 하하하하하하하하", recommendCount: 1000, createdAt: "2024-12-12T02:20:00.08Z", imageUrls: ["https://cdn.news.unn.net/news/photo/202110/516864_318701_956.jpg", "https://cdn.news.unn.net/news/photo/202110/516864_318701_956.jpg"], authorName: "정의찬", isLike: false, isPopular: true, isScrap: true)
         ]
     }
 }
